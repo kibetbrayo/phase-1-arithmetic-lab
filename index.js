87dbc491e86060a684fe7d187d4110c878965a04
iam <brian.kibet2@student.moringaschool.com>
2023-06-13 12:44:21 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+let multiply=num1*num2
+console.log(multiply);
+
+const random = Math.floor(Math.random()*100) +1;
+console.log(random);
+
+const num3=34
+const num4=6
+const mod = num3 % num4
+
+
+
+
+const set =[0, 6, 10, 12, 20, 16, 20, 8]
+const max = Math.max(...set);
+console.log(max);
